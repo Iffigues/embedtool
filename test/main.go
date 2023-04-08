692f6d42afa04a289b2d55247127b03c4ad6f201
iffigues <bordenoy@student.42.fr>
2023-04-08 22:16:49 +0200

    ez

--- test/main.go
+++ test/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"embedtool"
+	"fmt"
 )
-
 
 //go:embed hello
 var content embed.FS
 
 func main() {
-	fmt.Println(embedtool.CopyEmbededDir(content, "hello", "lml", true))
+	z := &embedtool.Tar{}
+	fmt.Println(embedtool.IsDir("hello/hello", content))
+	fmt.Println(z.Make(content, "hello", "dir.tar", false))
+	//fmt.Println(z.Make(content, "hello", "ici"))
+	//fmt.Println(embedtool.CopyEmbededDir(content, "hello", "lml", true))
 }
