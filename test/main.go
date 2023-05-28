50ef845c0e00f6698836daedd65a915a0625f7d8
iffigues <bordenoy@student.42.fr>
2023-05-28 04:11:14 +0200

    ez

--- test/main.go
+++ test/main.go
@@ -10,9 +10,6 @@ import (
 var content embed.FS
 
 func main() {
-	z := &embedtool.Tar{}
-	fmt.Println(embedtool.IsDir("hello/hello", content))
-	fmt.Println(z.Make(content, "hello", "dir.tar", false))
-	//fmt.Println(z.Make(content, "hello", "ici"))
-	//fmt.Println(embedtool.CopyEmbededDir(content, "hello", "lml", true))
+	fmt.Println("ezeez")
+	fmt.Println(embedtool.CopyEmbededDir(content, "hello", "./lml", true))
 }
